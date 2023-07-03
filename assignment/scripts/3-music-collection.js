29a567d138adca5f82efebaf282f320fd2ac0817
Zach Wright <zwright101@gmail.com>
2023-07-03 18:46:13 -0500

    got the first function working and albums added to my collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,50 @@
+// - Add a function named `findByArtist`. This function should:
+//   - Take in `artist` (a string) parameter
+//   - Create an array to hold any results, empty to start
+//   - Loop through the `collection` and add any objects with a matching artist to the array.
+//   - Return the array with the matching results. If no results are found, return an empty array.
+
+// - Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, 
+//   as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
+
+// > When testing your functions, write all tests in the JavaScript file!
+
+
 console.log('***** Music Collection *****')
+
+let collection = []; 
+
+function addToCollection(title, artist, yearPublished){
+    let albumToAdd = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    } 
+    collection.push(albumToAdd);
+    return albumToAdd; 
+}
+addToCollection("The Colour and the Shape", "The Foo Fighters", 1997);
+addToCollection("Echoes, Silence, Patience, and Grace", "The Foo Fighters", 2007);
+addToCollection("Meteora", "Linkin Park", 2003);
+addToCollection("Hybrid Theory", "Linkin Park", 2000);
+addToCollection("Woodgate, NY", "Novo Amor", 2014);
+addToCollection("Infamous", "Motionless in White", 2012);
+
+console.log(collection);
+console.log('Number of items in array:', collection.length);
+
+function showCollection(collection){
+    for (let i = 0; i < collection.length; i++){
+        console.log(collection[i].title, "by", collection[i].artist, "published in", collection[i].yearPublished);
+    }
+}
+showCollection(collection);
+
+let foundArtists = [];
+function findByArtist(){
+    for (let i = 0; i < collection.length; i++){
+        foundArtists.push(collection[i].title)
+        return foundArtists;
+    }
+}
+console.log(foundArtists);
